74db0060747e7f06c7a91379efd27c21b6f7743c
Gilberto Taccari <gilberto.taccari@gmail.com>
2020-03-14 12:41:55 +0100

    Setup a dummy Spring Boot service

---
+++ src/main/java/com/github/gilbertotcc/fbk/FbkBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.gilbertotcc.fbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbkBankingServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FbkBankingServiceApplication.class, args);
+  }
+
+}
